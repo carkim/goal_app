c1a0c2c352746d3be292f6c846767077285e4c41
Caroline <contentandform@gmail.com>
2015-12-15 19:26:57 -0500

    added deadline column to goals table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207221717) do
+ActiveRecord::Schema.define(version: 20151216000019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151207221717) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "deadline"
   end
 
   add_index "goals", ["user_id"], name: "index_goals_on_user_id", using: :btree
